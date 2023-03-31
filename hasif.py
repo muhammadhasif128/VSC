bb112cd11e3a503565d48e0195710263bc711ae3
Muhammad Hasif Bin Muhammad Imran <87602255+MuhammadHasif128@users.noreply.github.com>
2023-04-01 05:05:16 +0800

    taking a break

--- hasif.py
+++ hasif.py
@@ -1,2 +1,35 @@
-print("hello")
-print("lol")
+#inputs
+"""x = int(input("Enter a number: "))
+if x > 5:
+    print("Number is larger than 5")"""
+
+#variables
+"""a = "Muhammad"
+a = "Hasif" #will override
+print(a)"""
+
+#global functions, can use in functions
+"""x = "Hasif"
+def name():
+    print("Muhammad " + x)
+name()
+print("Who is " + x)"""
+
+#global keywords = once "global" is used, it can be used outside the function, not local anymore. global means it will use the variable outside function unless its not there.
+#if its not there, it will automatically make a local variable, global
+"""h = "hasif"
+def whatisyourname():
+    global h
+    h = "Hasif"
+whatisyourname()
+print("My name is " + h)"""
+
+#convert int to complex
+"""z = -5
+a = complex(z)
+print(a)"""
+
+#random, more at w3schools
+"""import random
+print(random.randrange(1,4))"""
+
